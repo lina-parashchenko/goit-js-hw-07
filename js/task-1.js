555de988a8601c1fc2fe3de6bb2a0d0481f2cbac
Lina Parashchenko <blond2109@gmail.com>
2025-02-15 23:38:47 +0200

    task1

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector("#categories");
+
+const categories = categoriesList.querySelectorAll(".item");
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+    const title = category.querySelector("h2").textContent;
+    const itemCount = category.querySelectorAll("ul li").length;
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${itemCount}`);
+});
+
